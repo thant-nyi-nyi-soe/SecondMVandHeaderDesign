dcab1853e16020d772efde1a776f302f3a3a7a72
thant_nyi <thantnyinyisoe.qualymm@gmail.com>
2025-07-03 14:27:55 +0630

    header fix 13

--- assets/js/template-part.js
+++ assets/js/template-part.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Header part
   const header = document.getElementById("headerPart");
   if (header) {
-    fetch("header.html")
+    fetch("/header.html")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
