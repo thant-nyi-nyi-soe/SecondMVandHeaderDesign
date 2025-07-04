e866bbe9defd9e98e0480547cfb8db80105f558d
thant_nyi <thantnyinyisoe.qualymm@gmail.com>
2025-07-04 15:19:50 +0630

    ./ js link

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,6 @@
 // Your JavaScript code goes here
 document.addEventListener("DOMContentLoaded", () => {});
 
-const menu = document.querySelector('tablet-menu-button');
-
-menu.addEventListener("on-click", () => {
-  console.log('works')
-});
-
 // Your jQuery code goes here
 $(function () {
   // Your code here
