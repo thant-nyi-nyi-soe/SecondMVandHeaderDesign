8f4367a4079655a3a46bee440ecd5c6a9bfee233
thant_nyi <thantnyinyisoe.qualymm@gmail.com>
2025-07-04 14:42:55 +0630

    started working on js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,12 @@
 // Your JavaScript code goes here
 document.addEventListener("DOMContentLoaded", () => {});
 
+const menu = document.querySelector('tablet-menu-button');
+
+menu.addEventListener("on-click", () => {
+  console.log('works')
+});
+
 // Your jQuery code goes here
 $(function () {
   // Your code here
