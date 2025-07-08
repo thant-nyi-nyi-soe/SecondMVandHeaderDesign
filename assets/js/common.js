6398f0e4047ca6a890fc3e9cb4f05f15425d12c4
thant_nyi <thantnyinyisoe.qualymm@gmail.com>
2025-07-08 10:03:08 +0630

    tablet page wirtten with js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,25 @@
 // Your JavaScript code goes here
-document.addEventListener("DOMContentLoaded", () => {});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const hamburger = document.querySelector('.tablet-menu-button');
+  const menu = document.querySelector('.menu');
+  const main = document.querySelector('.main');
+
+  hamburger.addEventListener("click", () => {
+    console.log('works');
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("show");
+
+    if (hamburger.classList.contains("active")) {
+      main.classList.add("hidden");
+    } else {
+      main.classList.remove("hidden");
+    }
+  });
+});
+
+
 
 // Your jQuery code goes here
 $(function () {
